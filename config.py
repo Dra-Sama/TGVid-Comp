b3c218fcf3b9af6fd74dd9ed8b67a55cf4ab5add
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-06-29 19:34:39 +0545

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "13986700")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "95d2e8a1aa81fc9b7fa8a8aeafe59537") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5625502158:AAGiG3fZqtdT9aOjgPHNr7BUFn4Un_qXOeE") # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', '-1001721659524') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
     # database config
-    DB_URL  = os.environ.get("DB_URL", "")  # ⚠️ Required
-    DB_NAME  = os.environ.get("DB_NAME","SnowEncoderBot") 
+    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://shankswillson33:shankswillson33@cluster0.vshfo6u.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
+    DB_NAME  = os.environ.get("DB_NAME","shankswillson33") 
 
     # Other Configs 
     ADMIN = int(os.environ.get("ADMIN", "0")) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '0')) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1001885135958')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
-    START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/414d09bd0cb43fed42e0a.jpg")
+    START_PIC = os.environ.get("START_PIC", "https://graph.org/file/4ac10607462346840a323.jpg")
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
